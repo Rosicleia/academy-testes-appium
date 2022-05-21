1c93fd64c986e2d9bb0690a32abc8cc088578e5a
Rosicleia <rosicleiasales@hotmail.com>
2022-05-20 21:24:37 -0300

    Atividade aula5

--- test/specs/mymovies.js
+++ test/specs/mymovies.js
@@ -8,20 +8,26 @@ describe("Primeiro teste", () => {
        await $("id=filter_image_button").click();
        const visibilidadeBotao = await $("id=clear_button").isDisplayed();
        expect(visibilidadeBotao).toBe(true);
-       
-       // Testar se o texto do botão de limpar filtro está correto
+       // Testar se o texto do botão de limpar filtro está correto - Feito por Rosicléia, Amanda, Anna Clara e Mateus Godoi
+       const textoBotao = await $("id=clear_button").getText();
+       expect(textoBotao).toBe("CLEAR");       
     });
 
     xit("Deve ser possível selecionar o gênero 'Action'", async () => {
         await $("id=filter_image_button").click();
         await $('android=new UiSelector().text("Action").className("android.widget.TextView")').click();
         await $("id=poster_image_view").click();
-
-        //verificar se um filme de ação foi exibido na tela de detalhe de filme
+        //verificar se um filme de ação foi exibido na tela de detalhe de filme - Feito por Rosicléia, Amanda, Anna Clara e Mateus Godoi
+        const nomeFilme = await $("id=title_text_view").getText();
+        expect(nomeFilme).toBe("Top Gun: Maverick");
+        
     });
 
-    xit("Deve ser possível verificar se a tela exibe a quantidade de estrelas do filme", () => {
+    it("Deve ser possível verificar se a tela exibe a quantidade de estrelas do filme", async () => {
         //verificar se após exibir o detalhe de um filme, a quantidade de estrelas é exibida.
+        const qtdEstrelas = await $("id=vote_average_rating_bar")
+
+        expect(qtdEstrelas).toBe(true);
     });
 
 });
